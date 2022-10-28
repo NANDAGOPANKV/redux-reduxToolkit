502f41d5bda8664873a538781c1df271dbbeca9d
nandagopan <nandagopankalpetta365@gmail.com>
2022-10-28 17:35:01 +0530

    again commit

--- src/components/cartbutton/AfterCartBtn.jsx
+++ src/components/cartbutton/AfterCartBtn.jsx
@@ -12,7 +12,7 @@ export const AfterCartBtn = () => {
     return (
         <div className='flex items-center justify-around '>
             {
-                setCount === 1 ? <p className='cursor-default mx-1 bg-rose-500 px-2 py-1 rounded ' onClick={() => dispatch(normal())} >X</p> : <button className='mx-1 bg-rose-500 px-3 py-1 rounded ' onClick={() => dispatch(decrement())} >-</button>
+                setCount === 1 ? <button className='mx-1 bg-rose-500 px-[9px] py-1 rounded ' onClick={() => dispatch(normal())} >X</button> : <button className='mx-1 bg-rose-500 px-3 py-1 rounded ' onClick={() => dispatch(decrement())} >-</button>
 
             }
             <div className='mx-1 cursor-default'  >{setCount}</div>
