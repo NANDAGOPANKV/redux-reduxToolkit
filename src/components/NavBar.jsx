906ff7a15c0cccaaefb290edbfa23ee2e020772f
nandagopan <nandagopankalpetta365@gmail.com>
2022-10-28 18:27:29 +0530

    navbar icons head cart item number

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { BsFillCartFill } from 'react-icons/bs'
+import { useSelector } from 'react-redux'
 
 export const NavBar = () => {
+
+    const { setCount } = useSelector((state) => state.cart)
+
     return (
         <div className='max-w-[2400px] h-[85px] bg-slate-700  pt-4'>
             <div className='flex items-center justify-between  w-full  px-[40px] '>
                 <h1 className='text-2xl font-bold cursor-default capitalize '>gopan's store</h1>
-                <div>
-                    <BsFillCartFill size={45} />
+                <div className='flex flex-col items-center'>
+                    <div>
+                        <p>{setCount === 0 ? null : setCount}</p>
+                    </div>
+                    <div>
+                        <BsFillCartFill size={45} />
+                    </div>
                 </div>
             </div>
 
